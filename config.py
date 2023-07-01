ea0649d9c8c6286831f3cf84683f741122ec146b
Marrivj00 <138113956+Marrivj00@users.noreply.github.com>
2023-07-01 14:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,12 +87,12 @@ Exᴀᴍᴩʟᴇ:- /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
     DEV_TXT = """<b><u>Sᴩᴇᴄɪᴀʟ Tʜᴀɴᴋꜱ & Dᴇᴠᴇʟᴏᴩᴇʀꜱ</b></u>
 » 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://t.me/Marri_Uchiha>MESSAGE ME GET</a>
 » 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://t.me/Marri_Uchiha>MAARI UCHIHA</a>
-• ❣️ <a href=https://github.com/lntechnical2>𝗟𝗡 𝗧𝗘𝗖𝗛𝗡𝗜𝗖</a>
-• ❣️ <a href=https://t.me/Mhd_rzn>𝗠𝗵𝗱_𝗿𝘇𝗻</a>
-• ❣️ <a href=https://youtu.be/GfulqsSnTv4>𝗠𝗼𝗧𝗲𝗰𝗵 𝗬𝗧</a>
-• ❣️ <a href=https://t.me/mr_MKN>𝗠𝗿.𝗠𝗞𝗡 𝗧𝗚</a>
-• ❣️ <a href=https://t.me/GitHub_noob>𝗚𝗶𝘁𝗛𝘂𝗯 𝗡𝗢𝗢𝗕</a>
-• ❣️ <a href=https://t.me/about_jeol>𝗝𝗲𝗼𝗹 𝗣𝗮𝘂𝗹</a> """
+• ❣️ <a href=https://t.me/Marri_Uchiha>PAID BOTS</a>
+• ❣️ <a href=https://t.me/Marri_Uchiha>MARRI UCHIHA</a>
+• ❣️ <a href=https://t.me/Marri_Uchiha>KOMBAN</a>
+• ❣️ <a href=https://t.me/Marri_Uchiha>UCHIHA</a>
+• ❣️ <a href=https://t.me/Marri_Uchiha>𝗚𝗶𝘁𝗛𝘂𝗯 𝗡𝗢𝗢𝗕</a>
+• ❣️ <a href=https://t.me/Marri_Uchiha>MARRI</a> """
 
     PROGRESS_BAR = """<b>\n
 ╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
